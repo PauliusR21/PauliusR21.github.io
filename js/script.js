170f5bd151beff1d4285225c0bd7e08cc2acc91e
PauliusR21 <34908571+PauliusR21@users.noreply.github.com>
2019-12-11 15:29:28 +0200

    Update script.js
    onscroll remove

--- js/script.js
+++ js/script.js
@@ -13,19 +13,7 @@ window.onload = function() { //executes this code after the DOM loads
      })
    }
  }
-//  NAV
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 // loading
 
 var myVar;
